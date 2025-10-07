172144b7644f457513dd75328e10c191e41eea0c
mrlop <mrlop@DELL-S>
2025-10-07 17:30:24 -0600

    Mensaje de bienvenida

--- Default.aspx.vb
+++ Default.aspx.vb
@@ -2,8 +2,6 @@
     Inherits Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
-        Dim p1 As New Persona()
-        Dim p2 As Persona(25)
-        Dim p3 As Persona("Juan", "Perez", 30)
+        lblNombre.Text = "Hola Mundo"
     End Sub
 End Class
