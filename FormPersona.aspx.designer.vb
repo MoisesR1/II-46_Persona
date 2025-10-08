a81e3488c91cc631c19793733e358add2dae5443
mrlop <mrlop@DELL-S>
2025-10-07 19:35:50 -0600

    Conexion BD

--- FormPersona.aspx.designer.vb
+++ FormPersona.aspx.designer.vb
@@ -21,4 +21,13 @@ Partial Public Class FormPersona
     '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
     '''</remarks>
     Protected WithEvents Gv_personas As Global.System.Web.UI.WebControls.GridView
+
+    '''<summary>
+    '''Control SqlDataSource.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents SqlDataSource As Global.System.Web.UI.WebControls.SqlDataSource
 End Class
